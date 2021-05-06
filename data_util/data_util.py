4625a3b47af0d880509da4e77d9dcbcde84e5e21
yanzhaochen <seechenyanzhao@gmail.com>
2021-05-06 22:36:26 +0800

    modify format

--- data_util/data_util.py
+++ data_util/data_util.py
@@ -19,33 +19,45 @@ def txt_to_pkl(txt_file, pkl_file):
         model=sentimodel, tokenizer=sentitokenizer, threshold=0.3
     )
 
-    utters = []
-    utter = []
+    conversations = []
+    conversation = []
     with open(txt_file, "rt") as f:
         for us_id, line in enumerate(f):
-            print(us_id, line)
+            # print(us_id, line)
+            # 0 'HeWentToJared91 ---+--- found out this morning i got a job promotion ! ! !\n'
             if line[0] != "*":
                 line = line.strip()
+
                 fields = line.split(" ---+--- ")
                 if len(fields) == 1:
                     print(us_id, fields[0])
                 if len(fields) == 2:
-                    print(us_id, "****" + fields[0] + "****", fields[1])
-                    sents = [fields[1].replace("'", "")]
-                    labeldict = goemotions(sents)
-                    pprint(labeldict)
-                    labels = labeldict[0]["labels"]
-                    print(labels)
-                    t = (fields[0], labels + sents)
-                    print(t)
-                    utter.append(t)
-            elif line[0] == "*":
-                utters.append(utter)
-                utter = []
+                    # print(us_id, "****" + fields[0] + "****", fields[1])
+                    # 0 ****HeWentToJared91**** found out this morning i got a job promotion ! ! !
 
-    print(utters)
+                    sents = [fields[1].replace("'", "")]
+                    # 'found out this morning i got a job promotion ! ! !'
+
+                    labeldict = goemotions(sents)
+                    # pprint(labeldict)
+                    # [{'labels': ['joy', 'neutral'], 'scores': [0.37095714, 0.5489171]}]
+
+                    labels = labeldict[0]["labels"]
+                    # print(labels)
+                    # ['joy', 'neutral']
+
+                    uttr = (fields[0], labels + sents)
+                    # print(uttr)
+                    # ('HeWentToJared91', ['joy', 'neutral', 'found out this morning i got a job promotion ! ! !'])
+                    conversation.append(uttr)
+
+            # end of conversation
+            elif line[0] == "*":
+                conversations.append(conversation)
+                conversation = []
+
     with open(pkl_file, "wb",) as ff:
-        pickle.dump(utters, ff)
+        pickle.dump(conversations, ff)
     print("finished")
 
 
